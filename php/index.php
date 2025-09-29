22ca22e58482ef00ddd6e4f9faaae10946127e74
Thiago <thiagozabala92@gmail.com>
2025-09-29 14:09:15 -0300

    Hi

--- php/index.php
+++ php/index.php
@@ -13,9 +13,17 @@
 </header>
 
 <body class="body_index">
-    <main class="selector">
-<a href="../php/usuarios/adscripta.php"><button type="button" class="btn btn-primary btn-lg"><img src="../img/icons/faceid_icon.png" class="indexlogo">Adscript@</button></a>
-<button id="btnEstudiante" type="button" class="btn btn-secondary btn-lg"><img src="../img/icons/student_icon.png" class="indexlogo">Estudiante</button>
+<main class="selector">
+
+    <div class="bienvenida">
+    <h2>Bienvenid@!</h2>
+    <h5>Selecciona una opción</h5>
+    </div>
+
+    <div class="botones">
+    <a href="../php/usuarios/adscripta.php"><button type="button" class="btn btn-primary btn-lg"><img src="../img/icons/faceid_icon.png" class="indexlogo">Adscript@</button></a>
+    <a href=""><button id="btnEstudiante" type="button" class="btn btn-secondary btn-lg"><img src="../img/icons/student_icon.png" class="indexlogo">Estudiante</button></a>
+    </div>
 </main>
 
 <div id="selectContainer" style="display:none; margin-top:20px;">
--- php/tools/header_adscripta.php
+++ php/tools/header_adscripta.php
@@ -30,7 +30,7 @@
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Cuenta</a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Cerrar Sesión</a></li>
+            <li><a class="dropdown-item" href="../index.php">Cerrar Sesión</a></li>
           </ul>
         </li>
       </ul>
