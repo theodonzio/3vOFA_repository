98ea23314027da28f5154f7b37c735cf6114ed83
Thiago <thiagozabala92@gmail.com>
2025-09-29 11:35:10 -0300

    fixing again

--- php/index.php
+++ php/index.php
@@ -2,19 +2,19 @@
   include '../php/tools/head.php';
 ?>
 
-<a href="../php/usuarios/adscripta.php"><button type="button" class="btn btn-primary btn-lg"><img src="../img/icons/faceid_icon.png" alt="">Adscripta</button></a>
-
-<button id="btnEstudiante" type="button" class="btn btn-secondary btn-lg"><img src="../img/icons/student_icon.png" alt="">Estudiante</button>
+<button type="button" class="btn btn-primary btn-lg"><img src="../img/icons/faceid_icon.png" class="indexlogo">Adscripta</button><
+<button id="btnEstudiante" type="button" class="btn btn-secondary btn-lg"><img src="../img/icons/student_icon.png" class="indexlogo">Estudiante</button>
 
 <div id="selectContainer" style="display:none; margin-top:20px;">
-  <select id="opcionesEstudiante" class="form-select">
+  
+<select id="opcionesEstudiante" class="form-select">
     <option value="">-- Selecciona una opción --</option>
     <option value="pagina1.php">Opción 1</option>
     <option value="pagina2.php">Opción 2</option>
     <option value="pagina3.php">Opción 3</option>
-  </select>
+</select>
 
-  <button id="redirigirBtn" class="btn btn-primary mt-2">Ir</button>
+    <button id="redirigirBtn" class="btn btn-primary mt-2">Ir</button>
 </div>
 
 <!-- Enlazar tu script JS externo -->
