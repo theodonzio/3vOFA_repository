7667b773430e25260d9a068e86be03dcbd248086
Thiago <thiagozabala92@gmail.com>
2025-09-29 13:44:40 -0300

    ADD stuff

--- php/index.php
+++ php/index.php
@@ -3,6 +3,7 @@
 ?>
     <link rel="stylesheet" href="../css/style.css">
 
+
 <header>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
@@ -11,20 +12,22 @@
     </div>
 </header>
 
-<main class="selector">
-<a href="../php/usuarios/adscripta.php"><button type="button" class="btn btn-primary btn-lg"><img src="../img/icons/faceid_icon.png" class="indexlogo">Adscripta</button></a>
+<body class="body_index">
+    <main class="selector">
+<a href="../php/usuarios/adscripta.php"><button type="button" class="btn btn-primary btn-lg"><img src="../img/icons/faceid_icon.png" class="indexlogo">Adscript@</button></a>
 <button id="btnEstudiante" type="button" class="btn btn-secondary btn-lg"><img src="../img/icons/student_icon.png" class="indexlogo">Estudiante</button>
 </main>
 
 <div id="selectContainer" style="display:none; margin-top:20px;">
 <select id="opcionesEstudiante" class="form-select">
-    <option value="">-- Selecciona una opción --</option>
     <option value="pagina1.php">Opción 1</option>
     <option value="pagina2.php">Opción 2</option>
     <option value="pagina3.php">Opción 3</option>
 </select>
     <button id="redirigirBtn" class="btn btn-primary mt-2">Ir</button>
 </div>
+
+</body>
 
 <!-- Enlazar tu script JS externo -->
 <script src="../js/select_estudiante.js"></script>
