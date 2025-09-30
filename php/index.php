98ce3f1bf33663c800059e449a476b786d9cf9a4
Thiago <thiagozabala92@gmail.com>
2025-09-30 10:30:06 -0300

    Modales

--- php/index.php
+++ php/index.php
@@ -1,8 +1,8 @@
 <?php
   include '../php/tools/head.php';
 ?>
-    <link rel="stylesheet" href="../css/style.css">
 
+<link rel="stylesheet" href="../css/style.css">
 
 <header>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
@@ -17,13 +17,92 @@
 
     <div class="bienvenida">
     <h2 class="font-weight-bold">Bienvenid@</h2>
-    <h5>Selecciona una opción</h5>
+    <h5>¿Quién eres?</h5>
     </div>
 
-    <div class="botones">
-    <a href="../php/usuarios/adscripta.php"><button type="button" class="btn btn-primary btn-lg"><img src="../img/icons/faceid_icon.png" class="indexlogo">Adscripto</button></a>
-    <a href="../php/usuarios/estudiante.php"><button id="btnEstudiante" type="button" class="btn btn-secondary btn-lg"><img src="../img/icons/student_icon.png" class="indexlogo">Estudiante</button></a>
-    <a href="../php/usuarios/docente.php"><button id="btnEstudiante" type="button" class="btn btn-secondary btn-lg text-white"><img src="../img/icons/book_icon.png" class="indexlogo">Docente</button></a>
+<div class="botones">
+  <!-- Botón Adscripta -->
+  <button type="button" class="btn btn-primary btn-lg" data-bs-toggle="modal" data-bs-target="#modalLogin" data-role="adscripta">
+    <img src="../img/icons/faceid_icon.png" class="indexlogo"> Adscripto
+  </button>
+
+  <!-- Botón Estudiante -->
+  <button id="btnEstudiante" type="button" class="btn btn-secondary btn-lg" data-bs-toggle="modal" data-bs-target="#modalEstudiante">
+    <img src="../img/icons/student_icon.png" class="indexlogo"> Estudiante
+  </button>
+
+  <!-- Botón Docente -->
+  <button type="button" class="btn btn-secondary btn-lg text-white" data-bs-toggle="modal" data-bs-target="#modalLogin" data-role="docente">
+    <img src="../img/icons/book_icon.png" class="indexlogo"> Docente
+  </button>
+</div>
+
+<!-- Modal Login (para Adscripta y Docente) -->
+<div class="modal fade" id="modalLogin" tabindex="-1" aria-labelledby="modalLoginLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalLoginLabel">Iniciar Sesión</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
+      </div>
+
+      <div class="modal-body">
+        <form id="formLogin" method="POST">
+          <div class="mb-3">
+            <label for="usuario" class="form-label">Usuario</label>
+            <input type="text" class="form-control" id="usuario" name="usuario" required>
+          </div>
+          <div class="mb-3">
+            <label for="contrasena" class="form-label">Contraseña</label>
+            <input type="password" class="form-control" id="contrasena" name="contrasena" required>
+          </div>
+          <!-- campo oculto para saber a dónde enviar -->
+          <input type="hidden" id="rol" name="rol">
+        </form>
+      </div>
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+        <button type="submit" form="formLogin" class="btn btn-primary">Ingresar</button>
+      </div>
+
     </div>
-</main>
+  </div>
+</div>
+
+<!-- Modal Estudiante -->
+<div class="modal fade" id="modalEstudiante" tabindex="-1" aria-labelledby="modalEstudianteLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalEstudianteLabel">Selecciona una opción</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
+      </div>
+
+      <div class="modal-body">
+        <form id="formEstudiante" action="../php/usuarios/estudiante.php" method="GET">
+          <div class="mb-3">
+            <label for="opcionEstudiante" class="form-label">Elige una opción</label>
+            <select class="form-select" id="opcionEstudiante" name="opcion">
+              <option value="" selected disabled>-- Selecciona --</option>
+              <option value="primaria">Primaria</option>
+              <option value="secundaria">Secundaria</option>
+              <option value="terciaria">Terciaria</option>
+            </select>
+          </div>
+        </form>
+      </div>
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+        <button type="submit" form="formEstudiante" class="btn btn-primary">Continuar</button>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
 </body>
