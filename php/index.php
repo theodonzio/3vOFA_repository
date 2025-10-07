1d4159b626aee49eaa98ab622acad3a543390512
Thiago <thiagozabala92@gmail.com>
2025-10-06 22:18:50 -0300

    things

--- php/index.php
+++ php/index.php
@@ -22,8 +22,8 @@
 
           <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="boton-tema">
             <li><h6 class="dropdown-header" data-traducible="Tema">Tema</h6></li>
-            <li><a class="dropdown-item" href="#" id="tema-claro" data-traducible="Claro">Claro</a></li>
-            <li><a class="dropdown-item" href="#" id="tema-oscuro" data-traducible="Oscuro">Oscuro</a></li>
+            <li><a class="dropdown-item" href="#" id="tema-claro" data-traducible="Claro"><img  class="icono">Claro</a></li>
+            <li><a class="dropdown-item" href="#" id="tema-oscuro" data-traducible="Oscuro"><img class="icono">Oscuro</a></li>
             <li><hr class="dropdown-divider"></li>
             <li><h6 class="dropdown-header" data-traducible="Lenguaje">Lenguaje</h6></li>
             <li><a class="dropdown-item" href="#" id="lenguaje-es" data-traducible="Español">Español</a></li>
