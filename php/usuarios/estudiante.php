4d8a040aee4204fd56a0561eeed87f6ca0be7ae0
Thiago <thiagozabala92@gmail.com>
2025-10-07 09:32:49 -0300

    en construccion

--- php/usuarios/estudiante.php
+++ php/usuarios/estudiante.php
@@ -6,10 +6,15 @@
   include '../tools/header_estudiante.php';
 ?>
 
-<?php
-  include '../tools/footer.php';
-?>
+<link rel="stylesheet" href="../css/style.css">
+
+<div class="en-construccion">
+  <i class="bi bi-cone-striped display-1 text-warning mb-3"></i>
+  <h2 class="fw-bold">En construcción</h2>
+  <p>Esta sección del estudiante aún se encuentra en desarrollo.</p>
+</div>
+
 
 <?php
-include '../tools/reloj.php';
+  include '../tools/footer.php';
 ?>
