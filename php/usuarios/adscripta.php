811b4880ce77a20a2f594e1c0d617744fdaf532b
Thiago <thiagozabala92@gmail.com>
2025-10-28 09:55:58 -0300

    nueva imagen recursos

--- php/usuarios/adscripta.php
+++ php/usuarios/adscripta.php
@@ -118,7 +118,7 @@ include '../funciones/limpiar_reservas_antiguas.php';
 
 <!-- Hero Recursos -->
 <div id="HeroRecursos" class="hero hero-imagen text-white py-5 d-flex align-items-center justify-content-center" 
-     style="background-image: url('https://images.unsplash.com/photo-1512314889357-e157c22f938d?q=80&w=2071&auto=format&fit=crop');">
+     style="background-image: url('https://images.unsplash.com/photo-1560169897-fc0cdbdfa4d5?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2072');">
     <div class="hero-overlay"></div>
     <div class="container text-center hero-content">
         <h2 class="display-6 fw-semibold">Recursos</h2>
